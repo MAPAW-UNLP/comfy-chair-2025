e8e8d2c4f9a422c37de59ad03474f66d2736ee2f
Martinez Rodrigo Javier <rodripincha7@gmail.com>
2025-09-30 12:08:35 -0300

    alta y edicion de conferencias

--- src/services/conferencias.ts
+++ src/services/conferencias.ts
@@ -2,14 +2,14 @@ import api from './api';
 
 type VISTA_CHOICES= "single blind" | "double blind" | "completo"
 
-  interface Conferencia {
-    id: string;
-    titulo: string;
-    descripcion: string;
-    fecha_ini: string;
-    fecha_fin: string;
-    vista: VISTA_CHOICES;
-  }
+export interface Conferencia {
+  id: string;
+  titulo: string;
+  descripcion: string;
+  fecha_ini: string;
+  fecha_fin: string;
+  vista: VISTA_CHOICES;
+}
 
 export const getAllConferencias = async (): Promise<Conferencia[]> => {
   const response = await api.get('/conferencias/');
